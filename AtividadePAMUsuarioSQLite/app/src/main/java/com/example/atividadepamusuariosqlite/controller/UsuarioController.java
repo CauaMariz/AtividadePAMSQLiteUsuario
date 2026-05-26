8167ec63b2c8e20b94d96ad86281fcf6b2dd61f4
Caua Mariz DEV <marizcaua88@gmail.com>
2026-05-26 11:16:12 -0300

    Add files via upload

---
+++ AtividadePAMUsuarioSQLite/app/src/main/java/com/example/atividadepamusuariosqlite/controller/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.example.atividadepamusuariosqlite.controller;
+
+import android.content.Context;
+
+import com.example.cauamarizatividadeiipamii.datasource.AppDataBase;
+
+
+public class UsuarioController extends AppDataBase {
+    public UsuarioController(Context context){
+        super(context);
+    }
+}
